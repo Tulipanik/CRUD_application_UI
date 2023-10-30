4b011346b9c93667679804dd100e35f21316ff4e
SzymonRogozinski <pokeszymon.pl@gmail.com>
2023-10-30 22:19:16 +0100

    Add creating tests

--- src/test/java/org/example/HttpRequest/HttpRequestServiceTest.java
+++ src/test/java/org/example/HttpRequest/HttpRequestServiceTest.java
@@ -47,4 +47,56 @@ public class HttpRequestServiceTest {
         //Then
     }
 
+    @Test
+    public void AddCorrectData() throws Exception {
+        //Give
+        String url="http://localhost:8080/notes";
+        String endpoint="";
+        String data="{\"title\":\"Title\",\"userId\":\"0\",\"content\":\"Simple note text\"}";
+        HttpRequestService httpRequestService=new HttpRequestService(url);
+        //When
+        boolean response=httpRequestService.postRequest(endpoint,data);
+        //Then
+        Assert.assertTrue(response);
+    }
+
+    @Test
+    public void AddInCorrectData() throws Exception {
+        //Give
+        String url="http://localhost:8080/notes";
+        String endpoint="";
+        String data="{\"tit    le\":\"Title\",\"use      rId\":\"0\"}";
+        HttpRequestService httpRequestService=new HttpRequestService(url);
+        //When
+        boolean response=httpRequestService.postRequest(endpoint,data);
+        //Then
+        Assert.assertFalse(response);
+    }
+
+    @Test
+    public void AddCorrectDataPermutation1() throws Exception {
+        //Give
+        String url="http://localhost:8080/notes";
+        String endpoint="";
+        String data="{\"userId\":\"0\",\"content\":\"Simple note text\",\"title\":\"Title\"}";
+        HttpRequestService httpRequestService=new HttpRequestService(url);
+        //When
+        boolean response=httpRequestService.postRequest(endpoint,data);
+        //Then
+        Assert.assertTrue(response);
+    }
+
+    @Test
+    public void AddCorrectDataPermutation2() throws Exception {
+        //Give
+        String url="http://localhost:8080/notes";
+        String endpoint="";
+        String data="{\"content\":\"Simple note text\",\"title\":\"Title\",\"userId\":\"0\"}";
+        HttpRequestService httpRequestService=new HttpRequestService(url);
+        //When
+        boolean response=httpRequestService.postRequest(endpoint,data);
+        //Then
+        Assert.assertTrue(response);
+    }
+
 }
