b73a477b6f03e3fd3ffd6acc1b2a1838c5ab48a5
MilczarekJan <101673349+MilczarekJan@users.noreply.github.com>
2023-11-04 00:12:01 +0100

    NotesServiceTest Delete chyba ok

--- src/test/java/org/example/HttpRequest/NotesServiceTest.java
+++ src/test/java/org/example/HttpRequest/NotesServiceTest.java
@@ -26,15 +26,140 @@ public class NotesServiceTest {
     public void DeleteAllNotes() throws Exception {
         //Given
         NotesService notesService = new NotesService();
-        String title = "Title";
-        String groupId = "0";
-        String content = "Simple note text";
+        String title1 = "Title1";
+        String groupId1 = "0";
+        String content1 = "Simple note text 1";
+        String title2 = "Title2";
+        String groupId2 = "1";
+        String content2 = "Simple note text 2";
         // When
-        notesService.add(title, content, groupId);
+        notesService.add(title1, content1, groupId1);
+        notesService.add(title2, content2, groupId2);
         notesService.deleteAllNotes();
         String response = notesService.getAllNotes();
         //Then
         Assert.assertEquals("[]", response);
+    }
+
+    @Test
+    public void DeleteNotesGroupByCorrectId() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title1 = "Title1";
+        String groupId1 = "0";
+        String content1 = "Simple note text 1";
+        String title2 = "Title2";
+        String groupId2 = "1";
+        String content2 = "Simple note text 2";
+        // When
+        notesService.deleteAllNotes();
+        notesService.add(title1, content1, groupId1);
+        notesService.add(title2, content2, groupId2);
+        notesService.deleteAllGroupNotes(groupId1);
+        String response1 = notesService.getGroupNotes(groupId1);
+        String response2 = notesService.getGroupNotes(groupId2);
+        //Then
+        String pattern = "\\{\"id\":\"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\",\"title\":\"Title2\",\"content\":\"Simple note text 2\",\"userId\":\"1\"\\}";
+        Pattern regexPattern = Pattern.compile(pattern);
+        Matcher matcher = regexPattern.matcher(response2);
+        Assert.assertEquals("[]", response1);
+        Assert.assertTrue(matcher.find());
+    }
+
+    @Test
+    public void DeleteNotesGroupByIncorrectId() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title1 = "Title1";
+        String groupId1 = "0";
+        String content1 = "Simple note text 1";
+        String title2 = "Title2";
+        String groupId2 = "1";
+        String content2 = "Simple note text 2";
+        // When
+        notesService.deleteAllNotes();
+        notesService.add(title1, content1, groupId1);
+        notesService.add(title2, content2, groupId2);
+        notesService.deleteAllGroupNotes("2");
+        String response1 = notesService.getGroupNotes(groupId1);
+        String response2 = notesService.getGroupNotes(groupId2);
+        //Then
+        Assert.assertNotEquals("[]", response1);
+    }
+
+    @Test
+    public void DeleteNotesGroupByIncorrectIdPerm1() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title1 = "Title1";
+        String groupId1 = "0";
+        String content1 = "Simple note text 1";
+        String title2 = "Title2";
+        String groupId2 = "1";
+        String content2 = "Simple note text 2";
+        // When
+        notesService.deleteAllNotes();
+        notesService.add(title1, content1, groupId1);
+        notesService.add(title2, content2, groupId2);
+        notesService.deleteAllGroupNotes("X");
+        String response1 = notesService.getGroupNotes(groupId1);
+        String response2 = notesService.getGroupNotes(groupId2);
+        //Then
+        Assert.assertNotEquals("[]", response1);
+    }
+
+    @Test
+    public void DeleteNoteByCorrectId() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title1 = "Title1";
+        String groupId1 = "0";
+        String content1 = "Simple note text 1";
+        String title2 = "Title2";
+        String groupId2 = "1";
+        String content2 = "Simple note text 2";
+        //When
+        notesService.deleteAllNotes();
+        notesService.add(title1, content1, groupId1);
+        notesService.add(title2, content2, groupId2);
+        String response = notesService.getAllNotes();
+        String pattern = "([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})";
+        Pattern regexPattern = Pattern.compile(pattern);
+        Matcher matcher = regexPattern.matcher(response);
+        boolean patternFound = matcher.find();
+        String idValue = matcher.group(0);
+        notesService.deleteChosenNote(idValue);
+        String deletedResponse = notesService.getIdNote(idValue);
+        //Then
+        Assert.assertTrue(patternFound);
+        Assert.assertEquals("", deletedResponse);
+    }
+
+    @Test
+    public void DeleteNoteByIncorrectId() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title1 = "Title1";
+        String groupId1 = "0";
+        String content1 = "Simple note text 1";
+        String title2 = "Title2";
+        String groupId2 = "1";
+        String content2 = "Simple note text 2";
+        //When
+        notesService.deleteAllNotes();
+        notesService.add(title1, content1, groupId1);
+        notesService.add(title2, content2, groupId2);
+        String response = notesService.getAllNotes();
+        String pattern = "([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})";
+        Pattern regexPattern = Pattern.compile(pattern);
+        Matcher matcher = regexPattern.matcher(response);
+        boolean patternFound = matcher.find();
+        String idValue = matcher.group(0);
+        notesService.deleteChosenNote("incorrectid");
+        String deletedResponse = notesService.getIdNote(idValue);
+        //Then
+        Assert.assertTrue(patternFound);
+        Assert.assertNotEquals("", deletedResponse);
     }
     //Getting method tests
     @Test
