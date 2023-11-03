800a3ff1ddd9916f0cbe7e442f053ee759119a6e
MilczarekJan <101673349+MilczarekJan@users.noreply.github.com>
2023-11-03 19:46:51 +0100

    Testy Delete i Read

--- src/test/java/org/example/HttpRequest/NotesServiceTest.java
+++ src/test/java/org/example/HttpRequest/NotesServiceTest.java
@@ -4,6 +4,9 @@ import org.example.Services.NotesService;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NotesServiceTest {
 
     //Class initialisation test
@@ -18,6 +21,77 @@ public class NotesServiceTest {
     }
 
     //CRUD method tests
+    //Deleting method tests
+    @Test
+    public void DeleteAllNotes() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title = "Title";
+        String groupId = "0";
+        String content = "Simple note text";
+        // When
+        notesService.add(title, content, groupId);
+        notesService.deleteAllNotes();
+        String response = notesService.getAllNotes();
+        //Then
+        Assert.assertEquals("[]", response);
+    }
+    //Getting method tests
+    @Test
+    public void GetAllNotes() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title = "Title";
+        String groupId = "0";
+        String content = "Simple note text";
+        // When
+        notesService.deleteAllNotes();
+        notesService.add(title, content, groupId);
+        String response = notesService.getAllNotes();
+        // Then
+        Assert.assertNotNull(response);
+        String pattern = "\\{\"id\":\"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\",\"title\":\"Title\",\"content\":\"Simple note text\",\"userId\":\"0\"\\}";
+        Pattern regexPattern = Pattern.compile(pattern);
+        Matcher matcher = regexPattern.matcher(response);
+        Assert.assertTrue(matcher.find());
+    }
+
+    @Test
+    public void GetNoteByCorrectGroupId() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title = "CorrectIdNote";
+        String groupId = "5";
+        String content = "Simple note text";
+        //When
+        notesService.deleteAllNotes();
+        notesService.add(title, content, groupId);
+        String response = notesService.getGroupNotes(groupId);
+        //Then
+        Assert.assertNotNull(response);
+        String pattern = "\\{\"id\":\"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\",\"title\":\"CorrectIdNote\",\"content\":\"Simple note text\",\"userId\":\"5\"\\}";
+        Pattern regexPattern = Pattern.compile(pattern);
+        Matcher matcher = regexPattern.matcher(response);
+        Assert.assertTrue(matcher.find());
+    }
+
+    @Test
+    public void GetNoteByIncorrectGroupId() throws Exception {
+        //Given
+        NotesService notesService = new NotesService();
+        String title = "IncorrectIdNote";
+        String groupId = "5";
+        String content = "Simple note text";
+        String incorrectGroupId = "X";
+        //When
+        notesService.deleteAllNotes();
+        notesService.add(title, content, groupId);
+        String response = notesService.getGroupNotes(incorrectGroupId);
+        //Then
+        Assert.assertEquals("[]", response);
+    }
+
+
     //Adding method tests
 
     @Test
